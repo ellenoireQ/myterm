1fb96c06c48962598dd63063c056e6d245e87d27
ellenoireQ <ketchupnaiso@gmail.com>
2026-02-03 09:44:33 +0700

    feat: Create to initial reading terminal

--- src/Tterminal/tterminal.rs
+++ src/Tterminal/tterminal.rs
@@ -1,1 +1,17 @@
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct Ferminal {
+    pub c_iflag: u32,
+    pub c_oflag: u32,
+    pub c_cflag: u32,
+    pub c_lflag: u32,
+    pub c_line: u8,
+    pub c_cc: [u8; 32],
+    pub c_ispeed: u32,
+    pub c_ospeed: u32,
+}
 
+unsafe extern "C" {
+    pub unsafe fn tcgetattr(fd: i32, tterminal: *mut Ferminal) -> i32;
+    pub unsafe fn tcsetattr(fd: i32, optional_actions: i32, tterminal: *const Ferminal) -> i32;
+}
